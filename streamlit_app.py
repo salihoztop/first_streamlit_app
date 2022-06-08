8ea054124f74e84d453e8b31c81f8b61dcf41953
Salih Oztop <soztop@yahoo.com>
2022-06-08 23:36:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Kale, Spinach')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
